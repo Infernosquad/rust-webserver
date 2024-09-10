ff57c1195320f19743159881db257ea2908e6e32
infernosquad <infzanoza@gmail.com>
2024-09-10 14:08:40 +0400

    Start to work on async IO

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-use std::{env, io::{prelude::*, BufReader}, net::{TcpListener, TcpStream}};
+use std::{env, fs, io::{prelude::*, BufReader}, net::{TcpStream}, thread};
 use std::fs::File;
+use std::net::TcpListener;
 use std::path::Path;
+use std::time::Duration;
 use yaml_rust2::{Yaml, YamlLoader};
+use webserver_rust::ThreadPool;
 
 #[derive(Hash, Debug)]
 struct Config {
     base_url: String,
-    root: String
+    root: String,
 }
 
 fn parse_config(hash: &Yaml) -> Config {
@@ -14,7 +17,7 @@ fn parse_config(hash: &Yaml) -> Config {
     let root = hash["root"].as_str().unwrap().to_string();
     Config {
         base_url,
-        root
+        root,
     }
 }
 
@@ -30,10 +33,13 @@ fn main() {
 
     let mut file_content = String::new();
     File::open(config_path).expect("File not found").read_to_string(&mut file_content).unwrap();
-    let docs= YamlLoader::load_from_str(&mut file_content).expect("Invalid YAML");
+    let docs = YamlLoader::load_from_str(&mut file_content).expect("Invalid YAML");
     let collected_docs: Vec<Config> = docs.iter().enumerate().map(|(index, doc)| {
         return parse_config(&doc);
     }).collect();
+
+    let pool = ThreadPool::new(collected_docs.len());
+    println!("Config: {:?}", collected_docs);
 
     for config in collected_docs {
         let exists = Path::new(&config.root).exists();
@@ -41,20 +47,28 @@ fn main() {
             println!("Root path does not exist {}", config.root);
             return;
         }
-        let listener = TcpListener::bind(config.base_url.as_str()).unwrap();
-
-        for stream in listener.incoming() {
-            let stream = stream.unwrap();
-
-            println!("Connection established {}!", config.base_url.as_str());
-            handle_connection(stream);
-        }
+        pool.execute(|| {
+            handle_connection(config);
+        });
     }
-
+    println!("Shutting down.");
 }
 
-fn handle_connection(mut stream: TcpStream) {
+
+fn handle_connection(config: Config) {
+    let listener = TcpListener::bind(config.base_url.as_str()).unwrap();
+
+    println!("Server started at {}", config.base_url.as_str());
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_stream(stream)
+    }
+}
+
+fn handle_stream(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
+    println!("{:?}", buf_reader);
     let http_request: Vec<_> = buf_reader
         .lines()
         .map(|result| result.unwrap())
